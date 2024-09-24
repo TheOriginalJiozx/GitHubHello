61fa110485fa7bff31e81a892f65b9f3ed65d1f5
TheOriginalJiozx <omal0001@stud.kea.dk>
2024-09-24 14:26:04 +0200

    Initial commit

---
+++ src/main/java/githubhello/githubhello/GitHubHelloApplication.java
@@ -0,0 +1,13 @@
+package githubhello.githubhello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubHelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/githubhello/githubhello/GitHubHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package githubhello.githubhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
